982ca2ca2d60a9e2ab0945a894da46f49c56a6cb
Johnny <suiyi8760@sina.com>
2022-11-20 15:03:39 +0800

    feat: match demo

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-fn main() {
-    for i in 1..9 {
-        let mut line = "".to_string();
-        for j in 1..9 {
-            if j <= i {
-                line += &format!("{}*{}={};", i, j, i * j);
-            } else {
-                break;
-            }
-        }
-        println!("{}", line);
+struct UserId(i64);
+
+enum ESex {
+    OTHER,
+    MALE,
+    FEMALE,
+}
+
+struct User {
+    id: UserId,
+    name: String,
+    sex: ESex,
+}
+
+fn greet(user: &User) -> String {
+    match user.sex {
+        ESex::FEMALE => format!("good afternoon, miss {}", user.name),
+        ESex::MALE => format!("good afternoon, mr {}", user.name),
+        ESex::OTHER => format!("good afternoon, {}", user.name),
     }
+}
+
+fn main() {
+    let tony = User {
+        id: UserId(1),
+        name: String::from("Tony"),
+        sex: ESex::MALE,
+    };
+    let jenny = User {
+        id: UserId(2),
+        name: String::from("Jenny"),
+        sex: ESex::FEMALE,
+    };
+    println!("{}", greet(&tony));
+    println!("{}", greet(&jenny));
 }
