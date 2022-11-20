d7c48e440e2f502093341528d98ae1038cc49d23
Johnny <suiyi8760@sina.com>
2022-11-20 21:49:31 +0800

    feat: 区分模块

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,17 @@
-struct UserId(i64);
 
-enum ESex {
-    OTHER,
-    MALE,
-    FEMALE,
-}
+mod demo_match;
+mod demo_condition;
+mod demo_html2md;
 
-struct User {
-    id: UserId,
-    name: String,
-    sex: ESex,
-}
-
-fn greet(user: &User) -> String {
-    match user.sex {
-        ESex::FEMALE => format!("good afternoon, miss {}", user.name),
-        ESex::MALE => format!("good afternoon, mr {}", user.name),
-        ESex::OTHER => format!("good afternoon, {}", user.name),
-    }
-}
+use crate::demo_match::demo_match;
+use crate::demo_condition::demo_condition;
+use crate::demo_html2md::demo_html2md;
 
 fn main() {
-    let tony = User {
-        id: UserId(1),
-        name: String::from("Tony"),
-        sex: ESex::MALE,
-    };
-    let jenny = User {
-        id: UserId(2),
-        name: String::from("Jenny"),
-        sex: ESex::FEMALE,
-    };
-    println!("{}", greet(&tony));
-    println!("{}", greet(&jenny));
+    println!("match模式匹配--------------------------------");
+    demo_match();
+    println!("条件控制--------------------------------");
+    demo_condition();
+    println!("html2md(简单体验rust开发流程)--------------------------------");
+    demo_html2md();
 }
